62e968ad5e67717385605dee614fec6cb6b898a6
akucher <aleksey.kucher@noveogroup.com>
2021-06-21 15:08:48 +0700

    Fixed

--- js/salaryViewer.js
+++ js/salaryViewer.js
@@ -241,7 +241,7 @@
         transformToTableRows(row, _tableData);
       });
 
-      console.log(tableData);
+      console.log(_tableData);
 
       _tableData = _tableData.sort((a, b) => (Date.parse(a.date) - Date.parse(b.date)));
       
